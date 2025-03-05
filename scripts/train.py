6d328d09aa32fc20357e899a1e231ceaea5cfd0e
jiadonghe007 <jiadonghe007@outlook.com>
2025-03-05 22:06:18 +0800

    支持命令行参数输入

--- scripts/train.py
+++ scripts/train.py
@@ -1,18 +1,40 @@
+from dataclasses import dataclass
+
 import gymnasium as gym
+import tyro
 
 from decision_transformer.model import DecisionTransformerAlgo
 
-env = gym.make("HalfCheetah-v4")
 
-dt_algo = DecisionTransformerAlgo(
-    env,
-    trajectories_path="data/halfcheetah-sb3ppo-1m-steps",
-    embedding_dim=128,
-    context_length=20,
-    num_gpt_layers=3,
-    num_head=1,
-    lr=1e-4,
-    batch_size=64,
-)
+@dataclass
+class Args:
+    env: str = "HalfCheetah-v4"  # Environment name
+    trajectories_path: str = "data/halfcheetah-medium-v2"  # Trajectory dataset path
+    train_iters: int = 20000  # Number of training iterations
+    expected_return: int = 6000  # Expected return
+    num_head: int = 4  # Number of heads in the multi-head attention
+    embedding_dim: int = 128  # Embedding dimension
+    context_length: int = 20  # DT context length
+    num_gpt_layers: int = 3  # Number of GPT layers
+    lr: float = 1e-4  # Learning rate
+    batch_size: int = 64  # Batch size
 
-dt_algo.learn(20000, expected_return=6000)
+
+def train(args):
+    env = gym.make(args.env)
+    dt_algo = DecisionTransformerAlgo(
+        env,
+        trajectories_path=args.trajectories_path,
+        embedding_dim=args.embedding_dim,
+        context_length=args.context_length,
+        num_gpt_layers=args.num_gpt_layers,
+        num_head=args.num_head,
+        lr=args.lr,
+        batch_size=args.batch_size,
+    )
+
+    dt_algo.learn(args.train_iters, expected_return=args.expected_return)
+
+
+args = tyro.cli(Args)
+train(args)
